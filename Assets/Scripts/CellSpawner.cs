a6bc092bf2c4a00b330477cf5284cda108c7338e
Isak Horvath <isakhorvath@gmail.com>
2022-09-07 19:38:41 +0200

    FIX: Add heart region graphics

--- Assets/Scripts/CellSpawner.cs
+++ Assets/Scripts/CellSpawner.cs
@@ -6,7 +6,6 @@ public class CellSpawner : MonoBehaviour
 {
     public GameObject cellObject;
     public GameObject heartObject;
-    public Transform playerPosition;
     public int distance = 5;
     public int amountX = 20;
     public int amountY = 20;
@@ -15,15 +14,13 @@ public class CellSpawner : MonoBehaviour
     {
         float scale = cellObject.GetComponent<Transform>().localScale.x;
         Heart heart = heartObject.GetComponent<Heart>();
-        float playerX = playerPosition.position.x;
-        float playerY = playerPosition.position.y;
         int startX = amountX/2;
         int startY = amountY/2;
         for (int x = -startX; x < startX; x++)
         {
             for (int y = -startY; y < startY; y++)
             {
-                GameObject cell = Instantiate(cellObject, new Vector2(playerX + scale * x * distance , playerY + scale * y * distance), Quaternion.identity);
+                GameObject cell = Instantiate(cellObject, new Vector2(transform.position.x + scale * x * distance , transform.position.y + scale * y * distance), Quaternion.identity);
                 heart.AddCell(cell);
             }
         }        
