9e1549162c4e51c1a982861ea2c98a74254bf912
ToBadForYou <jonisfalk@hotmail.com>
2022-09-08 17:11:59 +0200

    NEW: Detector now spawns antibodies

--- Assets/Scripts/Detector.cs
+++ Assets/Scripts/Detector.cs
@@ -6,7 +6,9 @@ public class Detector : RangedUnit
 {
     public GameObject reportTo;
     public GameObject scoutObject;
+    public GameObject antiBodyObject;
     public bool isAlerted;
+    public int maxAntiBodies = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +19,21 @@ public class Detector : RangedUnit
     public override void Attack(){
         if (!isAlerted && target.tag == "Player"){
             isAlerted = true;
-            SpawnScout();
+            SpawnUnits();
         }
     }
 
-    void SpawnScout(){
+    void SpawnUnits(){
         GameObject newScout = Instantiate(scoutObject, transform.position, Quaternion.identity);
         Scout scout = newScout.transform.Find("scoutRange").gameObject.GetComponent<Scout>();
         scout.parent = reportTo;
         scout.SetAlerted();
+
+        int antiBodiesAmount = Random.Range(2, maxAntiBodies);
+        for (int i = 0; i < antiBodiesAmount; i++)
+        {
+            GameObject newAntiBody = Instantiate(antiBodyObject, new Vector2(transform.position.x + Random.Range(-1.0f, 1.0f), transform.position.y + Random.Range(-1.0f, 1.0f)), Quaternion.identity);
+        }
     }
 
     void StopAttack(){
--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -9,18 +9,16 @@ public class UnitMovement : MonoBehaviour
     public float speed = 2.0f; // TODO Read speed value from owner object
     public bool moving;
 
-    // Start is called before the first frame update
     void Start()
     {
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(target != null && moving) {
             float step = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target.transform.position, step);
-            if(Vector2.Distance(transform.position, target.transform.position) <= 0.01f) {
+            if(Vector2.Distance(transform.position, target.transform.position) <= 0.2f) {
                 moving = false;
                 owner.OnReachedDestination(target);
             }
