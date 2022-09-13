31dcce45dbfc65ba6b0b082bc6027d3f29376603
Isak Horvath <isakhorvath@gmail.com>
2022-09-13 16:25:12 +0200

    FIX: Refactor blink effect code

---
+++ Assets/Scripts/EffectScripts/BlinkEffect.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkEffect : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    
+    private float multiplier = 1.0f;
+    private float maxAlpha = 1.0f;
+    private float minAlpha = 0.0f;
+
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void InvertMultiplier() {
+        multiplier *= -1;
+    }
+
+    void SetAlpha(float alpha) {
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, alpha);
+    }
+
+    void Update()
+    {
+        float newAlpha = sr.color.a + Time.deltaTime * multiplier;
+        SetAlpha(newAlpha);
+
+        if(sr.color.a > maxAlpha) {
+            SetAlpha(maxAlpha);
+            InvertMultiplier();
+        }
+        else if(sr.color.a < minAlpha) {
+            SetAlpha(minAlpha);
+            InvertMultiplier();
+        }
+    }
+}
