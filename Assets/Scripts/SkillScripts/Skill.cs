e4ce96297576a9e18a193c5d40f4db5464f2ffbd
ToBadForYou <jonisfalk@hotmail.com>
2022-09-25 10:41:41 +0200

    FIX: Skill tree

--- Assets/Scripts/SkillScripts/Skill.cs
+++ Assets/Scripts/SkillScripts/Skill.cs
@@ -1,51 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Skill : MonoBehaviour
+public class Skill : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public bool isActive;
     public int cost;
     public Skill prevSkill;
-    public SpriteRenderer sr;
+    public Image sr;
+    public bool hovering;
 
     public float effect;
 
-    void OnMouseEnter() {
+    public void OnPointerEnter(PointerEventData eventData) {
         if(!isActive && prevSkill.isActive) {
             sr.color = new Color(1.0f, 1.0f, 1.0f, 0.8f);
         }
     }
 
-    void OnMouseOver() {
+    public void Upgrade() {
         if(!isActive && prevSkill.isActive) {
-            if(Input.GetMouseButtonDown(0)) {
-                GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-                if(gm.DNAPoints >= cost) {
-                    gm.DNAPoints -= cost;
-                    isActive = true;
-                    sr.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+            if(gm.DNAPoints >= cost) {
+                gm.DNAPoints -= cost;
+                isActive = true;
+                sr.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
-                    Skilltree tree = GameObject.Find("skilltree").GetComponent<Skilltree>();
-                    switch (gameObject.name)
-                    {
-                        case "hp":
-                            tree.activeHPSkill = this;
-                            break;
-                        case "speed":
-                            tree.activeSpeedSkill = this;
-                            break;
-                        case "infection":
-                            tree.activeInfectionRateSkill = this;
-                            break;
-                    }
-                    tree.NotifyPlayer();
+                Skilltree tree = GameObject.Find("skilltree").GetComponent<Skilltree>();
+                switch (gameObject.name)
+                {
+                    case "hp":
+                        tree.activeHPSkill = this;
+                        break;
+                    case "speed":
+                        tree.activeSpeedSkill = this;
+                        break;
+                    case "infection":
+                        tree.activeInfectionRateSkill = this;
+                        break;
                 }
-            }
+                tree.NotifyPlayer();
+            }  
         }
     }
 
-    void OnMouseExit() {
+    public void OnPointerExit(PointerEventData eventData) {
         if(!isActive && prevSkill.isActive) {
             sr.color = new Color(1.0f, 1.0f, 1.0f, 0.4f);
         }
