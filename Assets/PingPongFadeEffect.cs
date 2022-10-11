fe62c497f64d00df538730ab3e3630f77de46b26
Isak Horvath <isakhorvath@gmail.com>
2022-10-11 10:14:14 +0200

    NEW: Create logo and 'finish' title screen

---
+++ Assets/PingPongFadeEffect.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PingPongFadeEffect : MonoBehaviour
+{
+    bool fadeOut = true;
+    TextMeshProUGUI textMesh;
+
+    void Start() {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if(fadeOut) {
+            textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, textMesh.color.a - Time.deltaTime);
+            if(textMesh.color.a <= 0.0f) {
+                fadeOut = false;
+            }
+        }
+        else {
+            textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, textMesh.color.a + Time.deltaTime);
+            if(textMesh.color.a >= 1.0f) {
+                fadeOut = true;
+            }
+        }
+    }
+}
