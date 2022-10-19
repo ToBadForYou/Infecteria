0492bb04ea7f550ee71fede639001aa86d11e81c
Jonathan Falk <jonisfalk@hotmail.com>
2022-10-19 12:08:17 +0200

    NEW: UnitType None added (for units not possible to produce)

--- Assets/Scripts/UnitScripts/UnitType.cs
+++ Assets/Scripts/UnitScripts/UnitType.cs
@@ -2,4 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum UnitType { ANTIBODY, MICROBACTERIA, SCOUT };
+public enum UnitType { ANTIBODY, MICROBACTERIA, SCOUT, NONE };
