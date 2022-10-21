d39937807faa1f5a34efcae055943620a869d565
Isak Horvath <isakhorvath@gmail.com>
2022-10-21 17:03:52 +0200

    NEW: Hopfully finished first quest, my computer is about to die

--- Assets/Quest.cs
+++ Assets/Quest.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Quest : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI progressMesh;
+    [SerializeField] Organ organ;
+
+    [SerializeField] int maxAmount;
+
+    public bool isFinished;
 
     void Update()
     {
-        
+        if(!isFinished) {
+            progressMesh.text = (maxAmount - organ.cells.Count) + "/" + maxAmount;
+            if(progressMesh.text == maxAmount + "/" + maxAmount)
+                isFinished = true;
+        }
     }
 }
