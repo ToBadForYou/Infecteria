3f940d802fcb4ef1d9e45905f3d0502028ec014b
Jonathan Falk <jonisfalk@hotmail.com>
2022-10-25 09:27:19 +0200

    FIX: Updated instructions

--- Assets/Scripts/ManagerScripts/TutorialHandler.cs
+++ Assets/Scripts/ManagerScripts/TutorialHandler.cs
@@ -16,12 +16,12 @@ public class TutorialHandler : MonoBehaviour
         {EventType.Absorbed, "You just absorbed your first cell, congratulations! Absorbing cells will grant you DNA points and recover lost health."},
         {EventType.MadeFactory, "Congratulations, you just made your first factory! Factories can do many things, such as autoinfect cells, build structures and produce units. You may also upgrade it to unlock more building slots."},
         {EventType.EnteredCell, "You just entered a cell! By staying inside you will increase its infection level until it has been fully infected."},
-        {EventType.Microbacteria, "You just produced your first microbacteria! These are your friends. You can select them by holding left mouse button and draging over them."},
+        {EventType.Microbacteria, "You just produced your first microbacteria! These are your friends. You can select them by holding left mouse button and dragging over them."},
         {EventType.ScoutDetected, "A scout has spotted you! Deal with it before it can report your presence to the heart!"},
         {EventType.DetectorDetected, "A detector has spotted you! A few antibodies has been deployed to deal with you, but most importantly a scout which will inform the heart about your presence unless you deal with it."},
         {EventType.CollectedSugar, "You just obtained your first sugar! Sugar can be used to upgrade factories and build structures inside your factories."},
         {EventType.CollectedDNA, "You just obtained your first DNA Point! DNA Points can be used in the SkillTree (T) to mutate your bacteria."},
-        {EventType.UnitSelected, "You just selected your first unit! From here you can control their stances (defensive/aggressive), move them (right click), make them follow you (F), and make them return to their factory (R)."},
+        {EventType.UnitSelected, "You just selected your first unit! From here you can control their stances (aggressive/defensive), move them (right click), make them follow you (F), and make them return to their factory (R)."},
     };
 
     public void TriggerEvent(EventType type) {
